e2adcc6c62d7aeb99735d68646e2f3c67a62459f
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-10-06 12:04:47 +0300

    Add rect

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-console.log('ok');
-
 let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 document.body.appendChild(svg);
+
+var svgns = "http://www.w3.org/2000/svg";
+var rect = document.createElementNS(svgns, 'rect');
+rect.setAttribute('x', '150');
+rect.setAttribute('y', '150');
+rect.setAttribute('height', '50');
+rect.setAttribute('width', '50');
+rect.setAttribute('fill', 'blue');
+svg.appendChild(rect);
