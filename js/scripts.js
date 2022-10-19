a44c24c0ecd0eaabfe806a1ef389a1ad73eba68d
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-10-19 09:18:34 +0300

    Create mkRect()

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,18 @@ let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 document.body.appendChild(svg);
 
 var svgns = "http://www.w3.org/2000/svg";
-var rect = document.createElementNS(svgns, 'rect');
-rect.setAttribute('x', '150');
-rect.setAttribute('y', '150');
-rect.setAttribute('height', '50');
-rect.setAttribute('width', '50');
-rect.setAttribute('fill', 'blue');
+let rect = mkRect();
 svg.appendChild(rect);
+
+/**
+ * Create new svg rect
+ */
+function mkRect() {
+    var rect = document.createElementNS(svgns, 'rect');
+    rect.setAttribute('x', '150');
+    rect.setAttribute('y', '150');
+    rect.setAttribute('height', '50');
+    rect.setAttribute('width', '50');
+    rect.setAttribute('fill', 'blue');
+    return rect;
+}
