22fa680e211ea8de001cef2ae6e2171a231d0b15
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-10-19 09:23:23 +0300

    Create mkSvg()

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+let svg = mkSvg();
 document.body.appendChild(svg);
 
-var svgns = "http://www.w3.org/2000/svg";
 let rect = mkRect();
 svg.appendChild(rect);
 
@@ -9,11 +8,17 @@ svg.appendChild(rect);
  * Create new svg rect
  */
 function mkRect() {
-    var rect = document.createElementNS(svgns, 'rect');
-    rect.setAttribute('x', '20');
-    rect.setAttribute('y', '20');
-    rect.setAttribute('height', '50');
-    rect.setAttribute('width', '50');
-    rect.setAttribute('fill', 'blue');
+    var rect = document.createElementNS(svgns, "rect");
+    rect.setAttribute("x", "20");
+    rect.setAttribute("y", "20");
+    rect.setAttribute("height", "50");
+    rect.setAttribute("width", "50");
+    rect.setAttribute("fill", "blue");
     return rect;
+}
+
+function mkSvg() {
+    let ns = "http://www.w3.org/2000/svg";
+    let svg = document.createElementNS(ns, "svg");
+    return svg;
 }
