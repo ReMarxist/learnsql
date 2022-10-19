fd20f4c0c7d5a53dedfb68e1429b578d2cba0927
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-10-19 10:08:26 +0300

    Move fns to svg.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,3 @@ document.body.appendChild(svg);
 let rect = mkRect();
 svg.appendChild(rect);
 addAnimation(rect);
-
-/**
- * Create svg
- */
-function mkSvg() {
-    const ns = "http://www.w3.org/2000/svg";
-    let svg = document.createElementNS(ns, "svg");
-    svg.style.borderColor = "#1a73e8";
-    svg.style.borderStyle = "solid";
-    svg.style.borderWidth = "1px";
-    return svg;
-}
-
-/**
- * Add animation to `rect`
- * @param {SVGRect} rect 
- */
-function addAnimation(rect) {
-    const ns = "http://www.w3.org/2000/svg";
-    let animate = document.createElementNS(ns, "animate");
-    animate.setAttribute("attributeName", "x");
-    animate.setAttribute("values", "20;80");
-    animate.setAttribute("dur", "3s");
-    animate.setAttribute("repeatCount", "1");
-    rect.setAttribute("x", "80");
-    rect.appendChild(animate);
-}
--- js/svg.js
+++ js/svg.js
@@ -1,4 +1,16 @@
 /**
+ * Create svg
+ */
+ function mkSvg() {
+    const ns = "http://www.w3.org/2000/svg";
+    let svg = document.createElementNS(ns, "svg");
+    svg.style.borderColor = "#1a73e8";
+    svg.style.borderStyle = "solid";
+    svg.style.borderWidth = "1px";
+    return svg;
+}
+
+/**
  * Create svg rect
  */
  function mkRect() {
@@ -11,4 +23,19 @@
     rect.setAttribute("stroke", "#1a73e8");
     rect.setAttribute("rx", "5");
     return rect;
+}
+
+/**
+ * Add animation to `rect`
+ * @param {SVGRect} rect 
+ */
+function addAnimation(rect) {
+    const ns = "http://www.w3.org/2000/svg";
+    let animate = document.createElementNS(ns, "animate");
+    animate.setAttribute("attributeName", "x");
+    animate.setAttribute("values", "20;80");
+    animate.setAttribute("dur", "3s");
+    animate.setAttribute("repeatCount", "1");
+    rect.setAttribute("x", "80");
+    rect.appendChild(animate);
 }
