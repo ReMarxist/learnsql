0cbab90abdd79541d9cd563a71a2548b51f62cd8
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-11-10 09:49:33 +0300

    Add click event

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,6 @@ let rect = addRect(svg, { x: 10, y: 50 });
 addAnimation(rect);
 let button = addRect(svg, { x: 100, y: 100 });
 button.style.cursor = "pointer";
+document.addEventListener("click", () => {
+    console.log("ok");
+});
