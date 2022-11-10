4601de6646e2f2f7b45f2363d53cf36c14e1b5a6
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-11-10 09:55:47 +0300

    Remove animation by default

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let svg = addSvg();
 let rect = addRect(svg, { x: 10, y: 50 });
-addAnimation(rect);
+// addAnimation(rect);
 let button = addRect(svg, { x: 100, y: 100 });
 button.style.cursor = "pointer";
 document.addEventListener("click", () => {
