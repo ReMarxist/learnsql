f0c78f1cb78a7d77d301ffc770f29cf797c227e5
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-11-21 09:55:15 +0300

    Move fns to table.js

--- js/svg.js
+++ js/svg.js
@@ -130,76 +130,11 @@ function removeAnimate(rect) {
 }
 
 /**
- * Create svg table
- * @param {SVGSVGElement} svg
- * @param {string} name Table name
- * @param {Object} position Position of table
- * @param {string[]} headers Headers of table (names of columns)
- * @param {string[][]} dataRows Data that fills the table
+ * Get height of `<text>` element
+ * @param {SVGTextElement} text 
  */
-function addTable(svg, name, position, headers, dataRows) {
-    let texts = createTableTexts(headers, dataRows);
-    let tableWidth = addTableTexts(svg, texts, position, headers.length);
-    addTableLabel(svg, name, position, tableWidth);
-}
-
-/**
- * Create `<text>` tags for table
- * @param {string[]} headers 
- * @param {string[][]} dataRows 
- */
-function createTableTexts(headers, dataRows) {
-    let headerSvgs = headers.map(createText);
-    let dataSvgs = dataRows.flatMap(row => row.map(createText));
-    return headerSvgs.concat(dataSvgs);
-}
-
-/**
- * Calculate positions and add `<text>` to `svg`
- * @param {SVGSVGElement} svg
- * @param {SVGTextElement[]} texts 
- * @param {Object} basePosition Top-left dot of table
- * @param {number} basePosition.x
- * @param {number} basePosition.y
- * @param {number} nColumns Number of columns in table
- * @returns {number} Table width
- */
-function addTableTexts(svg, texts, basePosition, nColumns) {
-    texts.forEach(text => {
-        // Append texts to svg before calculating their sizes
-        svg.appendChild(text);
-    });
-    let columnWidths = getColumnWidths(texts, nColumns);
-    let columnOffsets = getColumnOffsets(columnWidths);
-    let rowHeight = getMaxHeight(texts);
-    texts.forEach((text, i) => {
-        const columnWidth = columnWidths[i % nColumns];
-        const columnOffset = columnOffsets[i % nColumns];
-        const x = basePosition.x + (columnWidth - getWidth(text)) / 2 + columnOffset;
-        text.setAttribute("x", x);
-
-        const labelHeight = 50;
-        const y = basePosition.y + labelHeight + Math.floor(i / nColumns) * rowHeight;
-        text.setAttribute("y", y);
-    });
-    return sum(columnWidths);
-}
-
-/**
- * Calculate column widths
- * @param {SVGTextElement} texts 
- * @param {number} nColumns 
- */
-function getColumnWidths(texts, nColumns) {
-    let widths = [];
-    for (let i = 0; i < nColumns; i++) {
-        widths.push(getWidth(texts[i]));
-    }
-    for (let i = nColumns; i < texts.length; i++) {
-        let width = getWidth(texts[i]);
-        widths[i % nColumns] = Math.max(widths[i % nColumns], width);
-    }
-    return widths;
+function getHeight(text) {
+    return text.getBoundingClientRect().height;
 }
 
 /**
@@ -208,49 +143,4 @@ function getColumnWidths(texts, nColumns) {
  */
 function getWidth(text) {
     return text.getBoundingClientRect().width;
-}
-
-/**
- * Get offset of columns
- * @param {number[]} columnWidths 
- */
-function getColumnOffsets(columnWidths) {
-    let offsets = [];
-    let offset = 0;
-    for (let i = 0; i < columnWidths.length; i++) {
-        offsets.push(offset);
-        offset += columnWidths[i];
-    }
-    return offsets;
-}
-
-/**
- * Add label for whole table
- * @param {SVGSVGElement} svg 
- * @param {string} name 
- * @param {Object} tablePosition Top-left dot of table
- * @param {number} tablePosition.x
- * @param {number} tablePosition.y
- * @param {number} tableWidth
- */
-function addTableLabel(svg, name, tablePosition, tableWidth) {
-    let position = movedHorizontally(tablePosition, tableWidth / 2);
-    addText(svg, "Table " + name, position);
-}
-
-/**
- * Get max height of `<text>`
- * @param {SVGTextElement[]} texts 
- */
-function getMaxHeight(texts) {
-    let heights = texts.map(getHeight);
-    return max(heights);
-}
-
-/**
- * Get height of `<text>` element
- * @param {SVGTextElement} text 
- */
-function getHeight(text) {
-    return text.getBoundingClientRect().height;
 }
