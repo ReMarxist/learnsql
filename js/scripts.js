fb9202d29d5b64e2f6ddbe385c06213f0a88c19a
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-11-24 10:21:44 +0300

    Shorten strings in table

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,19 @@ addTableScenario();
 function addTableScenario() {
     let headers = ["Rank", "Company", "Country", "Industry", "Revenue"];
     let dataRows = [
-        ["1", "Walmart", "United States", "Retail", "572.8"],
-        ["2", "Amazon", "United States", "Internet Services and Retailing", "469.8"],
-        ["3", "State Grid", "China", "Energy", "460.6"],
-        ["4", "China National Petroleum", "China", "Petroleum", "411.7"],
-        ["5", "Sinopec Group", "China", "Petroleum", "401.3"],
-        ["6", "Saudi Aramco", "Saudi Arabia", "Energy",	"400.4"],
-        ["7", "Apple", "United States", "Technology", "365.8"],
-        ["8", "Volkswagen", "Germany", "Automobiles", "295.8"],
-        ["9", "China Construction", "China", "Engineering",	"293.7"],
-        ["10", "CVS Health", "United States", "Health care", "292.1"]
+        ["1", "Walmart", "United States", "Retail", "573"],
+        ["2", "Amazon", "United States", "E-commerce", "470"],
+        ["3", "State Grid", "China", "Energy", "461"],
+        ["4", "China Petroleum", "China", "Petroleum", "412"],
+        ["5", "Sinopec Group", "China", "Petroleum", "401"],
+        ["6", "Saudi Aramco", "Saudi Arabia", "Energy",	"400"],
+        ["7", "Apple", "United States", "Technology", "366"],
+        ["8", "Volkswagen", "Germany", "Automobiles", "296"],
+        ["9", "China Construction", "China", "Engineering",	"294"],
+        ["10", "CVS Health", "United States", "Health care", "292"]
     ];
 
-    addTable(svg, "FortuneGlobal", { x: 50, y: 50 }, headers, dataRows);
+    addTable(svg, "Corporations", { x: 50, y: 50 }, headers, dataRows);
 }
 
 function addControlButtonScenario() {
