53947a1f7908a6e4a870fd8440345f505481cf17
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-11-28 10:27:23 +0300

    Style query top border

--- js/query.js
+++ js/query.js
@@ -2,8 +2,10 @@ function addQueryInput() {
     let svg = createSvgElement("svg");
     svg.style.position = "fixed";
     svg.style.left = "0";
-    svg.style.bottom = "-100px";
+    svg.style.bottom = "0";
     svg.style.width = "100%";
     svg.style.height = "100px";
+    svg.style.backgroundColor = "white";
+    svg.style.borderTop = "3px #eaeaea solid";
     document.body.appendChild(svg);
 }
