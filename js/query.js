b856f7f1a1cad17ddec8e9c31b946b15bef09dd4
ReMarxist <111741772+ReMarxist@users.noreply.github.com>
2022-11-28 10:18:51 +0300

    Resize query svg

--- js/query.js
+++ js/query.js
@@ -1,4 +1,9 @@
 function addQueryInput() {
     let svg = createSvgElement("svg");
+    svg.style.position = "fixed";
+    svg.style.left = "0";
+    svg.style.bottom = "-100px";
+    svg.style.width = "100%";
+    svg.style.height = "100px";
     document.body.appendChild(svg);
 }
